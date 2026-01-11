3543ee1c5c3c77f5420621616d4903983da4d311
Gabriel Janjic <janjicgabriel1@gmail.com>
2026-01-11 19:33:33 +0100

    fixed pagination 2.0

--- components/PaginationQuery.tsx
+++ components/PaginationQuery.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { IoArrowBack, IoArrowForward } from "react-icons/io5";
 
 const PaginationQuery = ({
   pageNum,
@@ -11,6 +12,13 @@ const PaginationQuery = ({
   path1: string;
   query: string;
 }) => {
+  const pageNumbers: number[] = [];
+
+  for (let i = pageNum - 2; i <= pageNum + 2; i++) {
+    if (i > 0 && i <= totalPages) {
+      pageNumbers.push(i);
+    }
+  }
   return (
     <div className="flex gap-4 justify-center items-center mt-6 pb-4">
       {pageNum > 1 && (
@@ -18,18 +26,32 @@ const PaginationQuery = ({
           href={`${path1}?page=${pageNum - 1}&query=${query}`}
           className="px-3 py-1 bg-gray-200 rounded"
         >
-          Prev
+          <div className="flex gap-1 items-center">
+            <IoArrowBack />
+            Prev
+          </div>
         </Link>
       )}
-      <span className="px-3 py-1 whitespace-nowrap">
-        Page {pageNum} / {totalPages}
-      </span>
+      {pageNumbers.map((num) => (
+        <Link
+          key={num}
+          href={`${path1}?page=${num}&query=${query}`}
+          className={`px-3 py-1 rounded ${
+            num === pageNum ? "bg-blue-500 text-white" : "bg-gray-200"
+          }`}
+        >
+          {num}
+        </Link>
+      ))}
       {pageNum < totalPages && (
         <Link
           href={`${path1}?page=${pageNum + 1}&query=${query}`}
           className="px-3 py-1 bg-gray-200 rounded"
         >
-          Next
+          <div className="flex gap-1 items-center">
+            Next
+            <IoArrowForward />
+          </div>
         </Link>
       )}
     </div>
